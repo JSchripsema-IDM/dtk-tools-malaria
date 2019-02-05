d5533da2ef589fd03e60349677de9e7614c8b7b7
JSchripsema-IDM <jschripsema@idmod.org>
2019-02-04 16:48:36 -0800

    finished health_seeking intervention

--- malaria/interventions/health_seeking.py
+++ malaria/interventions/health_seeking.py
@@ -28,34 +28,49 @@ def add_health_seeking(config_builder,
                        broadcast_event_name='Received_Treatment'):
 
     """
+    Add health-seeking behavior when a triggering event occurs using 
+    the **NodeLevelHealthTriggeredIV** class. By default, triggered
+    when new clinical or severe malaria cases occur.
+
     Args:
-        config_builder: The :py:class:`DTKConfigBuilder <dtk.utils.core.DTKConfigBuilder>` containing the campaign configuration
-        start_day: Day we want to start the intervention
-        targets: The different targets held in a list of dictionaries (see default for example)
-        drug: The drug to administer
-            Format: str for a single drug or list of str for multiple drugs
-        dosing: The dosing for the drugs
-        nodes: nodes to target.
-            All nodes: {"class": "NodeSetAll"}.
-            Subset of nodes: {"class": "NodeSetNodeList", "Node_List": list_of_nodeIDs}
-        node_property_restrictions: used with NodePropertyRestrictions.
-            Format: list of dicts: [{ "NodeProperty1" : "PropertyValue1" }, {'NodeProperty2': "PropertyValue2"}, ...]
-        ind_property_restrictions: used with Property_Restrictions_Within_Node.
-            Format: list of dicts: [{ "IndividualProperty1" : "PropertyValue1" }, {'IndividualProperty2': "PropertyValue2"}, ...]
-        disqualifying_properties: A list of Individual Property Key:Value pairs that cause an intervention to be aborted
-            Format: list of strings: ["IndividualProperty1:PropertyValue1"]
-        drug_ineligibility_duration: if this param is > 0, use IndividualProperties to prevent people from receiving
-            drugs too frequently. Demographics file will need to define the IP DrugStatus with possible values None and
-            RecentDrug. Individuals who receive drugs for treatment will have their DrugStatus changed to RecentDrug for
-            drug_ineligibility_duration days. Individuals who already have status RecentDrug will not receive drugs for
-            treatment.
-        duration: how long the intervention lasts.
-        repetitions: Number repetitions.
-        tsteps_btwn_repetitions: Timesteps between the repetitions.
-        broadcast_event_name: Broadcast event.
+        config_builder: The :py:class:`DTKConfigBuilder <dtk.utils.core.DTKConfigBuilder>` 
+          containing the campaign configuration.
+        start_day: The day on which to start distributing the intervention 
+          (**Start_Day** parameter).
+        targets: A list of dictionaries that targets individuals based on 
+          triggering events. See the default for an example. The product of 
+          coverage and seek is the value for the **Demographic_Coverage** 
+          parameter. 
+        drug: The drug to administer. Use a str for a single drug or
+          a list of str for multiple drugs.
+        dosing: The dosing for the drugs (**Dosing_Type** parameter).
+        nodes: The dictionary of nodes to apply this intervention to. 
+          For all nodes: ``{"class": "NodeSetAll"}``. For a subset 
+          of nodes: ``{"class": "NodeSetNodeList", "Node_List": list_of_nodeIDs}``.
+        node_property_restrictions: The node properties a node must have 
+          to receive the intervention (**Node_Property_Restrictions** parameter).
+          Formatted as a list of dicts: ``[{ "NodeProperty1" : "PropertyValue1" }, 
+          {'NodeProperty2': "PropertyValue2"}, ...]``.
+        ind_property_restrictions: The individual properties an individual
+          must have to receive the intervention (**Property_Restrictions_Within_Node 
+          parameter). Formatted as a list of dicts: ``[{ "IndividualProperty1" : "PropertyValue1" }, 
+          {'IndividualProperty2': "PropertyValue2"}, ...]``.
+        disqualifying_properties: The individual properties that an individual 
+          cannot have to receive the intervention. Formatted as a list of strings: 
+          ``["IndividualProperty1:PropertyValue1"]``.
+        drug_ineligibility_duration: The number of days to change the individual
+          property value of **DrugStatus** to RecentDrug after receiving an 
+          intervention. Individuals with status RecentDrug will not receive
+          drugs for treatment. The demographics file must define the IP 
+          **DrugStatus** with possible values None and RecentDrug. 
+        duration: The length of the intervention, in days.
+        repetitions: The number of times to repeat the intervention.
+        tsteps_btwn_repetitions: The time steps between the repetitions.
+        broadcast_event_name: The event name that is broadcast (**Broadcast_Event**
+          parameter).
 
     Returns:
-
+      None
     """
 
     receiving_drugs_event = {
@@ -131,6 +146,47 @@ def add_health_seeking_by_chw(config_builder,
                               drug_ineligibility_duration=0,
                               duration=100000,
                               chw={}):
+
+    """
+    Add health-seeking behavior when a triggering event occurs using 
+    the **CommunityHealthWorkerEventCoordinator** class. By default, triggered
+    when new clinical or severe malaria cases occur.
+
+    Args:
+        config_builder: The :py:class:`DTKConfigBuilder <dtk.utils.core.DTKConfigBuilder>` 
+          containing the campaign configuration.
+        start_day: The day on which to start distributing the intervention 
+          (**Start_Day** parameter).
+        targets: A list of dictionaries that targets individuals based on 
+          triggering events. See the default for an example. The product of 
+          coverage and seek is the value for the **Demographic_Coverage** 
+          parameter. 
+        drug: The drug to administer. Use a str for a single drug or
+          a list of str for multiple drugs.
+        dosing: The dosing for the drugs (**Dosing_Type** parameter).
+        nodeIDs: The list of nodes to apply this intervention to (**Node_List** 
+          parameter). For all nodes, NodeSetAll. For a subset 
+          of nodes, the list_of_nodeIDs.
+        node_property_restrictions: The node properties a node must have 
+          to receive the intervention (**Node_Property_Restrictions** parameter).
+          Formatted as a list of dicts: ``[{ "NodeProperty1" : "PropertyValue1" }, 
+          {'NodeProperty2': "PropertyValue2"}, ...]``.
+        ind_property_restrictions: The individual properties an individual
+          must have to receive the intervention (**Property_Restrictions_Within_Node 
+          parameter). Formatted as a list of dicts: ``[{ "IndividualProperty1" : "PropertyValue1" }, 
+          {'IndividualProperty2': "PropertyValue2"}, ...]``.
+        drug_ineligibility_duration: The number of days to change the individual
+          property value of **DrugStatus** to RecentDrug after receiving an 
+          intervention. Individuals with status RecentDrug will not receive
+          drugs for treatment. The demographics file must define the IP 
+          **DrugStatus** with possible values None and RecentDrug. 
+        duration: The length of the intervention, in days.
+        chw: The dictionary of the community health worker configuration
+          that is overlayed on the default.
+
+    Returns:
+      None
+    """
 
     chw_config = {
         'class': 'CommunityHealthWorkerEventCoordinator',
